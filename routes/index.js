a13deb1d0f967844e39c66294cdc003d0b1c8af0
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2020-06-15 20:24:22 -0400

    latest changes

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,50 @@
-var express = require("express");
-var router  = express.Router();
-var passport = require("passport");
-var User = require("../models/user");
+var express = require("express")
+var router = express.Router()
+var passport = require("passport")
+var User = require("../models/user")
 
-router.get("/", function(req, res){
-    res.render("landing");
-});
+router.get("/", function (req, res) {
+  res.render("landing")
+})
 
-router.get("/register",function(req, res) {
-    res.render("register");
-});
+router.get("/register", function (req, res) {
+  res.render("register")
+})
 
 //handle signup
-router.post("/register", function(req, res) {
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            req.flash("error", err.message);
-           return res.render("register");
-        } 
-            passport.authenticate("local")(req, res,function(){
-                req.flash("success", "Welcome to YelpCamp " + user.username);
-                res.redirect("/campgrounds");
-            });
-    });
-});
+router.post("/register", function (req, res) {
+  var newUser = new User({ username: req.body.username })
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      req.flash("error", err.message)
+      return res.render("register")
+    }
+    passport.authenticate("local")(req, res, function () {
+      req.flash("success", "Welcome to Go-Camping " + user.username)
+      res.redirect("/campgrounds")
+    })
+  })
+})
 
 // Add login route
-router.get("/login", function(req, res) {
-   res.render("login"); 
-});
+router.get("/login", function (req, res) {
+  res.render("login")
+})
 
-router.post("/login",passport.authenticate("local",
-{
+router.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/campgrounds",
-    failureRedirect: "/login"
-}),function(req, res){});
+    failureRedirect: "/login",
+  }),
+  function (req, res) {}
+)
 
 // LogOut route
-router.get("/logout", function(req, res) {
-    req.logout();
-    req.flash("success", "Successfuly Signed Out")
-    res.redirect("/campgrounds");
-});
+router.get("/logout", function (req, res) {
+  req.logout()
+  req.flash("success", "Successfuly Signed Out")
+  res.redirect("/campgrounds")
+})
 
-module.exports = router;
+module.exports = router
