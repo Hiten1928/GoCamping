25026b9286a93e55491e7607b00a403aebbfe54b
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2020-06-15 20:32:19 -0400

    update routes with changes and Google maps API

--- models/campground.js
+++ models/campground.js
@@ -2,25 +2,27 @@ var mongoose = require("mongoose")
 // Schema Setup
 
 var campgroundSchema = new mongoose.Schema({
-    name : String,
-    image : String,
-    description : String,
-    cost: Number,
-    location: String,
-    lat: Number,
-    lng: Number,
-    createdAt: { type: Date, default: Date.now },
-    author: {
-        id:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        }, 
-        username: String
+  name: String,
+  image: String,
+  description: String,
+  cost: Number,
+  location: String,
+  lat: Number,
+  lng: Number,
+  createdAt: { type: Date, default: Date.now },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-    comments : [{
-       type: mongoose.Schema.Types.ObjectId,
-       ref : "Comment"
-    }]
-});
+    username: String,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+})
 
-module.exports = mongoose.model("Campground", campgroundSchema);
+module.exports = mongoose.model("Campground", campgroundSchema)
